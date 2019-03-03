dc14182eb6b6461bc5dd335bf9731f8162ea1f48
Olivier Van Aken <olivier.va@outlook.com>
2019-03-03 14:12:32 +0100

    Added comments - child process is passed on, on resolve- exit code is passed on, on reject - removed default inherited stdio option - output is passed on to the console by default - added optional argument to turn off default output behaviour

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,46 @@ import { ChildProcess } from "child_process";
 
 import cs from 'cross-spawn';
 
-export async function async(command: string, args: string[] = []): Promise<any> {
+/**
+ * Spawns a command prefixed with 'npx' wrapped by a Promise.
+ *
+ * @param command The command prefixed with 'npx'
+ * @param args Child process args
+ * @param options Child process options
+ * @param output If true stdout and stderr of the ChildProcess will be passed on to the console.
+ * @returns Promise<any>
+ */
+export async function async(command: string, args: string[] = [], options: {} = {}, output: boolean = true): Promise<any> {
     await new Promise((resolve, reject): void => {
-        const child: ChildProcess = spawn(command, args, {
-            stdio: 'inherit'
-        });
+        const child: ChildProcess = spawn(command, args, options);
+
+        if (output) {
+            child.stdout.on('data', (data) => {
+                console.info(data);
+            });
+
+            child.stderr.on('data', (data) => {
+                console.error(data);
+            });
+        }
 
         child.once('exit', (code: number): void => {
-            if (code !== 0) reject();
-            resolve();
+            if (code !== 0) reject(code);
+            resolve(child);
         });
     });
 }
 
+async('lol');
+
+/**
+ * Spawns a command prefixed with 'npx'
+ *
+ * @param command The command prefixed with 'npx'
+ * @param args Child process args
+ * @param options Child process options
+ * @returns ChildProcess The resulting ChildProcess
+ */
 export function spawn(command: string, args: string[] = [], options: object = {}): ChildProcess {
     return cs.spawn('npx', [command, ...args], options);
 }
